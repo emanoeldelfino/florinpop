088bdde44e55c9c0fd0471c238e30b65a2ef095f
emanoeldelfino <emanoeldelfno@gmail.com>
2021-06-21 22:10:38 -0300

    Add bin2Dec project

---
+++ bin2Dec/app.js
@@ -0,0 +1,83 @@
+import bin2Dec from "./convertBin.js";
+
+const gitHubImg = document.querySelector("img#github");
+const inputField = document.querySelector("input#binary");
+const inputText = document.querySelector("span#input-text");
+const submitButton = document.querySelector("button#submit");
+const resultSpan = document.querySelector("div#result > span#converted-number");
+const resultText = document.querySelector("span#result-text");
+const tooltip = document.querySelector(".tooltip");
+console.log(tooltip);
+
+function evts(elem, properties, pair, change) {
+  const elemToChange = change || elem;
+  if (properties.length === 1) {
+    for (let [event, value] of Object.entries(pair)) {
+      elem.addEventListener(event, () => {
+        elemToChange[properties[0]] = value;
+      });
+    }
+  } else if (properties.length > 1) {
+    let elemProperty = elemToChange;
+    for (let i = 0; i < properties.length - 1; i++) {
+      elemProperty = elemProperty[properties[i]];
+    }
+
+    for (let [event, value] of Object.entries(pair)) {
+      elem.addEventListener(event, () => {
+        elemProperty[properties[properties.length - 1]] = value;
+      });
+    }
+  }
+}
+
+evts(gitHubImg, ["src"], {
+  mouseover: "./images/GitHub-Mark-Green-32px.png",
+  mouseout: "./images/GitHub-Mark-Light-32px.png",
+});
+
+evts(
+  inputField,
+  ["style", "color"],
+  { focus: "green", blur: "white" },
+  inputText
+);
+
+let decimalNum = 0;
+
+submitButton.addEventListener("click", () => {
+  const binaryNum = inputField.value;
+
+  try {
+    decimalNum = bin2Dec(binaryNum);
+    resultSpan.innerText = decimalNum;
+    resultSpan.style.color = 'green';
+  } catch(err) {
+    window.alert(err.message);
+    resultSpan.innerText = 'Invalid';
+    resultSpan.style.color = 'red';
+  }
+});
+
+resultText.addEventListener("click", () => {
+  copyToClipboard(decimalNum);
+});
+
+const copyToClipboard = str => {
+  const el = document.createElement('textarea');
+  el.value = str;
+  el.setAttribute('readonly', '');
+  el.style.position = 'absolute';
+  el.style.left = '-9999px';
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+
+  // tooltip
+  tooltip.style.display = 'inline-block';
+
+  window.setTimeout(() => {
+    tooltip.style.display = 'none';
+  }, 3000)
+};
---
+++ bin2Dec/convertBin.js
@@ -0,0 +1,24 @@
+export default function binToDec(bits) {
+  const invalidInput = [..."23456789"].some((num) => bits.includes(num));
+
+  if (invalidInput) {
+    throw new Error(
+      "Invalid input. Binary numbers are composed only by 0s and 1s."
+    );
+  } else {
+    let negative = bits[0] === "-" ? true : false;
+
+    let total = 0;
+    const oneInd = bits.indexOf("1");
+
+    let bin = negative ? bits.slice(1) : bits;
+    bin = oneInd > 0 ? bits.slice(oneInd) : bits;
+
+    for (let bit of bin) {
+      total *= 2;
+      total += Number(bit);
+    }
+
+    return negative ? -Number(total) : Number(total);
+  }
+}
