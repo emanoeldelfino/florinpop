f9c17e93263b9d5564d7d5e2c83571b5207ef66a
emanoeldelfino <emanoeldelfno@gmail.com>
2021-06-26 12:00:35 -0300

    Minor fixes and udpate

--- christmasLights/app.js
+++ christmasLights/app.js
@@ -1,57 +1,108 @@
-const playPauseButton = document.querySelector("i#play-pause-button");
-
+const playPauseButton = document.querySelector("i#play-pause");
+const reloadButton = document.querySelector("i#reload");
 const sliderElem = document.querySelector("input#slider");
 
-intensity = sliderElem.value;
-sliderElem.addEventListener("change", () => {
-  intensity = sliderElem.value;
-});
+const colorInputs = document.querySelectorAll("input[type='color']");
+let lightsDefault = Array.from(document.querySelectorAll("label div.light"));
+let lights = [...lightsDefault];
 
-let lights = [1, 2, 3, 4, 5, 6, 7];
+function pauseLights() {
+  if (playPauseButton.className.includes("pause")) {
+    lights = lights.slice(-1).concat(lights.slice(0, -1));
 
-
-playPauseButton.addEventListener("click", (evt) => {
-  const [currentClass, replaceClass] = evt.target.className.includes("play")
-    ? ["play", "pause"]
-    : ["pause", "play"];
-  evt.target.className = evt.target.className.replace(
-    currentClass,
-    replaceClass
-  );
-
-  if (currentClass === "play") {
-    console.log(lights);
-    (function blinkLights() {
-      lights.forEach((light, index) => {
-        let lightElem = document.querySelector(`div#light${light}`);
-        // 0 0 50px color
-        setTimeout(() => {
-          lightElem.setAttribute(
-            "style",
-            `filter:brightness(100%); box-shadow:0 0 50px ${
-              getComputedStyle(lightElem)["background-color"]
-            };`
-          );
-          setTimeout(() => {
-            lightElem.setAttribute(
-              "style",
-              "filter:brightness(20%); box-shadow: none;"
-            );
-            lights = lights.slice(1).concat(lights[0]);
-          }, intensity);
-        }, intensity * index);
-      })
-      setTimeout(blinkLights, 7 * intensity);
-    })();
-  } else {
     // Set a fake timeout to get the highest timeout id
     let highestTimeoutId = setTimeout(";");
     for (let i = 0; i < highestTimeoutId; i++) {
       clearTimeout(i);
     }
-    for (let i = 1; i <= 7; i++) {
-      let lightElem = document.querySelector(`div#light${i}`);
-      lightElem.setAttribute("style", "filter:brightness(20%);");
-    }
+    playPauseButton.className = playPauseButton.className.replace("pause", "play");
+  }
+}
+
+function blinkLights() {
+  if (playPauseButton.className.includes("play")) {
+    playPauseButton.className = playPauseButton.className.replace("play", "pause");
+    setIntervalFirstNoDelay(() => {
+      lights.forEach((lightElem, index) => {
+        setTimeout(() => {
+          let color =
+            lightElem.style.backgroundColor === ""
+              ? getComputedStyle(lightElem)["background-color"]
+              : lightElem.style.backgroundColor;
+  
+          // turn on light
+          lightElem.setAttribute(
+            "style",
+            `background-color: ${color}; filter:brightness(100%); box-shadow:0 0 50px ${color};`
+          );
+  
+          // turn off light
+          setTimeout(() => {
+            lightElem.setAttribute("style", `background-color: ${color};`);
+          }, intensity);
+          lights = lights.slice(1).concat(lights[0]);
+        }, intensity * index);
+      });
+    }, 7 * intensity)
+  }
+}
+
+function resetLights() {
+  lights.forEach((lightElem) => {
+    lightElem.setAttribute(
+      "style",
+      `background-color: ${lightElem.style.backgroundColor};`
+    );
+  });
+  lights = [...lightsDefault];
+}
+
+reloadButton.addEventListener("click", () => {
+  pauseLights();
+  resetLights();
+});
+
+colorInputs.forEach((colorInput) => {
+  colorInput.addEventListener("change", (elem) => {
+    const prev = colorInput.previousElementSibling;
+    prev.style.backgroundColor = elem.target.value;
+  });
+});
+
+intensity = sliderElem.value;
+sliderElem.addEventListener("change", () => {
+  if (playPauseButton.className.includes("pause")) {
+    pauseLights();
+    intensity = sliderElem.value;
+    console.log(intensity);
+    blinkLights();
+  } else {
+    intensity = sliderElem.value;
   }
 });
+
+function toggleLights() {
+  const [currentClass, replaceClass] = playPauseButton.className.includes(
+    "play"
+  )
+    ? ["play", "pause"]
+    : ["pause", "play"];
+
+  if (currentClass === "play") {
+    blinkLights();
+  } else {
+    pauseLights();
+  }
+
+  playPauseButton.className = playPauseButton.className.replace(
+    currentClass,
+    replaceClass
+  );
+}
+
+playPauseButton.addEventListener("click", toggleLights);
+
+function setIntervalFirstNoDelay(callback, delay) {
+  callback();
+  setInterval(callback, delay);
+}
