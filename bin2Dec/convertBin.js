af53c016f77acb82dd7867364edccfd7a04ff0b5
emanoeldelfino <emanoeldelfno@gmail.com>
2021-10-30 14:44:45 -0300

    Update negative checking and fix incorrect variable name

--- bin2Dec/convertBin.js
+++ bin2Dec/convertBin.js
@@ -1,24 +1,17 @@
-export default function binToDec(bits) {
-  const invalidInput = [..."23456789"].some((num) => bits.includes(num));
-
-  if (invalidInput) {
+export default function bin2Dec(binary) {
+  if ([..."23456789"].some((num) => binary.includes(num))) {
     throw new Error(
       "Invalid input. Binary numbers are composed only by 0s and 1s."
     );
   } else {
-    let isNegative = bits[0] === "-" ? true : false; 
+    const bits = Math.abs(Number(binary));
 
     let total = 0;
-    const oneInd = bits.indexOf("1");
-
-    let bin = negative ? bits.slice(1) : bits;
-    bin = oneInd > 0 ? bits.slice(oneInd) : bits;
-
-    for (let bit of bin) {
+    for (let bit of String(bits)) {
       total *= 2;
       total += Number(bit);
     }
 
-    return isNegative ? -Number(total) : Number(total);
+    return bits === Number(binary) ? total : -total;
   }
 }
